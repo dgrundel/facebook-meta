936ce8072f88d259e456912acffe462562c90b0b
dgrundel <daniel@webpresencepartners.com>
2013-05-31 10:58:15 -0400

    first real commit

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ facebook-meta
 =============
 
 A simple, no frills Facebook meta tag generator for your WordPress site.
+
+Tags generated look like this:
+
+```
+<meta property="og:url" content="{your_post_permalink}"/>
+<meta property="og:title" content="{your_post_title}"/>
+<meta property="og:description" content="{your_post_excerpt}"/>
+<meta property="og:image" content="{your_post_featured_image_url}"/>
+```
+
+Meta tags are only generated for single posts/pages/etc. Basically, when [is_singular()](http://codex.wordpress.org/Function_Reference/is_singular) returns true. 
---
+++ facebook-meta.php
@@ -0,0 +1,27 @@
+<?php /*
+    Plugin Name: Facebook Meta
+    Plugin URI: http://webpresencepartners.com
+    Description: Adds Facebook Metadata
+    Version: 1.0
+    Author: Daniel Grundel, Web Presence Partners
+    Author URI: http://www.webpresencepartners.com
+*/
+
+    class FacebookMeta {
+        public function __construct() {
+            add_action('wp_head', array(&$this, 'meta_tags'));
+        }
+
+        public function meta_tags() {
+            if(is_singular()) {
+                global $post;
+                ?>
+                <meta property="og:url" content="<?php echo esc_attr(get_permalink($post->ID)); ?>"/>
+                <meta property="og:title" content="<?php echo esc_attr(get_the_title($post->ID)); ?>"/>
+                <meta property="og:description" content="<?php echo esc_attr(apply_filters('get_the_excerpt', $post->post_excerpt)); ?>"/>
+                <meta property="og:image" content="<?php echo esc_attr(wp_get_attachment_url(get_post_thumbnail_id($post->ID))); ?>"/>
+                <?php
+            }
+        }
+    }
+    new FacebookMeta();
